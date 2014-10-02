0d616c05e01cfb4a0562a7fcac09a02a8f5fe7a2
control9 <control9.lcme@gmail.com>
2014-10-02 21:02:31 +0400

    Login updated
    Added redirection after login, js moved from login.html.

---
+++ orders/js/login.js
@@ -0,0 +1,39 @@
+function auth() {
+	var data = $("#loginform").serializeArray();
+	$.ajax({
+		type: "POST",
+		url: "./backend/auth.php",
+		data: data,
+		success: function(data) {
+			var good = $.cookie('session') && $.cookie('id');
+			if (good) {
+				redirect();
+			}
+			else {
+				$(".alert").remove();
+				$('<div/>', {
+				class: 'alert alert-warning',
+				text: "Login failed"
+			}).insertAfter("#prompt");
+			}
+		},
+		error:  function(xhr, str){
+			$(".alert").remove();
+			$('<div/>', {
+				class: 'alert alert-danger',
+				text: "There are problems with server"
+			}).insertAfter("#prompt");
+		}
+	});
+}
+function redirect() {
+	var good = $.cookie('session') && $.cookie('id');
+	if (good) {
+		if  ($.cookie('worker') === '1') {
+			window.location.replace('./worker.html');
+		}
+		else if ($.cookie('worker') === '0') {
+			window.location.replace('./customer.html');
+		}
+	}
+}
